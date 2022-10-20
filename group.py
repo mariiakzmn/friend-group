b641c98a1f1cd80031e9fa8cd4f84312fe90c53f
Nikita Pond <nikita.pond@icloud.com>
2022-10-20 10:46:47 +0100

    Introduced initial class structure for group of people

--- group.py
+++ group.py
@@ -2,4 +2,41 @@
 
 # Your code to go here...
 
-my_group =
+
+class Person:
+    
+    def __init__(self, name, age, job=None, relationships=None):
+        self.name = name
+        self.age = age
+        self.job = job
+        
+        if relationships:
+            self.relationships =  relationships
+        else:
+            self.relationships = {}
+
+    
+    def set_relationship(self, person, relationship):
+        
+        self.relationships[person.name] = relationship
+        
+        if relationship == "friend":
+            person.relationships[self.name] = "friend"
+            
+        elif relationship == "parent":
+            person.relationships[self.name] = "kid"
+            
+        
+        pass
+        
+    
+        
+    
+
+my_group = [Person("Jill", 26, "biologist", relationships={"Zalika":"friend"}),
+            Person("Zalika", 28, "artist", relationships={"Jill":"friend"}),
+            Person("John", 27, "writer"),
+            Person("Nash", 34, "chef")     
+]
+
+my_group[0].set_relationship(my_group[1], "friend")
